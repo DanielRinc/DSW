60f04faf5a160fe0c379a08033c53802adbe0132
DanielRinc <91727951+DanielRinc@users.noreply.github.com>
2021-10-06 11:43:52 +0100

    Add files via upload

--- Horoscopo/horoscopo.php
+++ Horoscopo/horoscopo.php
@@ -1,18 +1,60 @@
 <?php
-    
 
+    $diaSeleccionado = empty ($_REQUEST['dias']) ? "0" : $_REQUEST['dias'];
+    
+    $mesSeleccionado = empty ($_REQUEST['mes']) ? "Seleccionar Mes" : $_REQUEST['mes'];
     function calcularHoroscopo($mes, $dia){
         $res = "";
-        if ($mes == 12 && $dia >= 21 || $mes == 1 && $dia <=19) {
-
+        if ($mes == 12 && $dia >= 22 || $mes == 1 && $dia <= 20) {
             $res = "Capricornio";
+        }else{
+            if ($mes == 1 && $dia >= 21 || $mes == 2 && $dia <= 19) {
+                $res = "Acuario";
+            }else{
+                if ($mes == 2 && $dia >= 20 || $mes == 3 && $dia <= 20) {
+                    $res = "Piscis";
+                }else{
+                    if ($mes == 3 && $dia >= 21 || $mes == 4 && $dia <= 20) {
+                        $res = "Aries";
+                    }else {
+                        if ($mes == 4 && $dia >= 21 || $mes == 5 && $dia <= 20) {
+                            $res = "Tauro";
+                        }else {
+                            if ($mes == 5 && $dia >= 21 || $mes == 6 && $dia <= 20) {
+                                $res = "Geminis";
+                            }else {
+                                if ($mes == 6 && $dia >= 21 || $mes == 7 && $dia <= 20) {
+                                    $res = "Cancer";
+                                }else {
+                                    if ($mes == 7 && $dia >= 21 || $mes == 8 && $dia <= 21) {
+                                        $res = "Leo";
+                                    }else {
+                                        if ($mes == 8 && $dia >= 22 || $mes == 9 && $dia <= 20) {
+                                            $res = "Virgo";
+                                        }else {
+                                            if ($mes == 9 && $dia >= 21 || $mes == 10 && $dia <= 20) {
+                                                $res = "Libra";
+                                            }else {
+                                                if ($mes == 10 && $dia >= 21 || $mes == 11 && $dia <= 21) {
+                                                    $res = "Escorpio";
+                                                }else {
+                                                    if ($mes == 11 && $dia >= 22 || $mes == 12 && $dia <= 21) {
+                                                        $res = "Sagitario";
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-
         return $res;
     }
     
-
     function mostrararray($array, $limite){
         /*
         foreach ($array as $key) {
@@ -24,7 +66,6 @@
         }
     }
     
-
     function diasSelect($array, $name, $limite){
         echo "<select name=\"$name\" id=\"$name\">";
         for ($i=0; $i < $limite; $i++) { 
@@ -59,9 +100,19 @@
 
 <?php
 
-    $meses = ["enero" => "31", "febrero" => "28", "marzo" => "31", "abril" => "30", "mayo" => "31",
+    /*
+    $arrayMeses = ["enero" => "31", "febrero" => "28", "marzo" => "31", "abril" => "30", "mayo" => "31",
      "junio" => "30", "julio" => "31", "agosto" => "31", "septiembre" => "30", "octubre" => "31", 
      "noviembre" => "30", "diciembre" => "31"];
+    */
+
+     $arrayMeses=['Enero' => 31,'Febrero' => 00,'Marzo' => 00,'Abril' => 00,'mayo' => 31,'Junio' => 00,'Julio' => 00,
+     'Agosto' => 00,'Septiembre' => 00,'Octubre' => 00,'Noviembre' => 00,'Diciembre' => 00];
+
+
+    $arrayMesesANumero = ["enero" => 1, "febrero" => 2, "marzo" => "3", "abril" => "4", "mayo" => "5",
+    "junio" => "6", "julio" => "7", "agosto" => "8", "septiembre" => "9", "octubre" => "10", 
+    "noviembre" => "11", "diciembre" => "12"];
 
 
     $horoscopos = ["aries", "tauro", "geminis", "cancer", 
@@ -72,20 +123,22 @@
         $dias[] = $i;
     }
     
-    
-
+    $result  = "Aun no se ha seleccionado";
+    //$result = empty ($_REQUEST['dias']) ? "Aun no se ha calculado" : $_REQUEST['dias'];
     if (isset($_GET["enviarMes"])) {
-        
         //crearSelect($horoscopos, "horoscopo");
-        //crearSelect($meses, "meses");
-        
-        $mesSeleccionado = $_REQUEST['mes'];
-        $conversor = $meses[$mesSeleccionado];
-        /*
-        diasSelect($dias, "dias", $conversor);
-        */
-        //echo "<h2><strong>".$mesSeleccionado,"</strong></h2>";
-        
+        //crearSelect($arrayMeses, "arrayMeses");
+        if (isset($_REQUEST['mes'])) { 
+            $mesSeleccionado = $_REQUEST['mes'];
+            if ($mesSeleccionado == "Seleccionar Mes") {
+                
+            }else{
+                $diasDelMes = $arrayMeses[$mesSeleccionado];
+            }
+            
+
+        }
+                
     }
 
 
@@ -109,8 +162,8 @@
     echo "</select>";
 
 
-    echo "<select name=\"meses\">";
-    foreach ($meses as $dato) {
+    echo "<select name=\"arrayMeses\">";
+    foreach ($arrayMeses as $dato) {
         echo "<option value=\"\">".$dato."</option>";
     }
     echo "</select>";
--- Horoscopo/horoscopo_vista.php
+++ Horoscopo/horoscopo_vista.php
@@ -5,44 +5,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <title>Horoscopo</title>
 </head>
 
 <body>
-
-
-
-    <div>
+    <div class="contorno">
         <form action="horoscopo.php" method="GET">
-            <button type="submit" name="enviarMes">Seleccionar Dia</button>
-            <button type="reset"><a href="horoscopo.php">Borrar</a></button>
-
-
-            <select name="mes" id="mes">
-                <option value="enero">enero</option>
-                <option value="febrero">febrero</option>
-                <option value="marzo">marzo</option>
-                <option value="abril">abril</option>
-                <option value="mayo">mayo</option>
-                <option value="junio">junio</option>
-                <option value="julio">julio</option>
-                <option value="agosto">agosto</option>
-                <option value="septiembre">septiembre</option>
-                <option value="octubre">octubre</option>
-                <option value="noviembre">noviembre</option>
-                <option value="diciembre">diciembre</option>
-            </select>
-            <?php
-                    
-                if (isset($_GET["enviarMes"])) {
-                    $mesSeleccionado = $_REQUEST['mes'];
-                    $conversor = $meses[$mesSeleccionado];
-                    diasSelect($dias, "dias", $conversor);
-                    echo $mesSeleccionado;
-                }
-
-            ?>
-            <button type="submit" name="averiguarHoroscopo">¿Cual es Mi Horoscopo?</button>
+            <div>
+                <select name="mes" id="mes">
+                    <option value= <?= $mesSeleccionado ?> selected ><?= $mesSeleccionado ?></option>
+                    <option value="enero">enero</option>
+                    <option value="febrero">febrero</option>
+                    <option value="marzo">marzo</option>
+                    <option value="abril">abril</option>
+                    <option value="mayo">mayo</option>
+                    <option value="junio">junio</option>
+                    <option value="julio">julio</option>
+                    <option value="agosto">agosto</option>
+                    <option value="septiembre">septiembre</option>
+                    <option value="octubre">octubre</option>
+                    <option value="noviembre">noviembre</option>
+                    <option value="diciembre">diciembre</option>
+                </select>
+            </div>
+            <div class="botonesPrincipales">
+                <button type="submit" name="enviarMes">Seleccionar Dia</button>
+                <button type="reset"><a href="horoscopo.php">Borrar</a></button>
+            </div>
+            <div>
+                <select name="dias" id="dias">
+                    <option value="<?= $diaSeleccionado?>" selected disabled hidden><?= $diaSeleccionado?></option>
+                    <?php
+                        if (isset($_GET["mes"])) {
+                            diasSelect2($dias, "dias", $diasDelMes);
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="botonesSecundarios">
+                <button type="submit" name="averiguarHoroscopo">¿Cual es Mi Horoscopo?</button>
+            </div>
+            <div>
+                <?php
+                        if (isset($_REQUEST['dias'])) {
+                            $diaSeleccionado = $_REQUEST['dias'];
+                            if (isset($_GET["averiguarHoroscopo"])) {
+                                $conversor = $arrayMesesANumero[$mesSeleccionado];
+                                $resultHoroscopo = calcularHoroscopo($conversor, $diaSeleccionado);
+                            }
+                        }
+                        
+                ?>
+                <!--Aqui ira el Horoscopo calculado-->
+                <!--en el value = <?php echo $result; ?>-->
+                <label for="result">Resultado</label>
+                <input type="text" name="result" value="<?php echo $resultHoroscopo = empty ($resultHoroscopo) ? "Aun no se ha seleccionado" : $resultHoroscopo; ?>">
+            </div>
+            
         </form>
     </div>
 
